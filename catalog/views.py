949b2c36d4f5590c521ccdb833b0ab592f4eda2f
Aleksey Manzhikov <sanal-manzhikov@mail.ru>
2024-08-01 01:19:39 +0300

    init

---
+++ catalog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        with open('catalog/text.txt', 'a', encoding='utf-8') as file:
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            file.write(f"Имя пользователя : {name}\nТелефон: {phone}\nСообщение: {message}\n")
+            print(name, phone, message)
+
+
+    return render(request, 'catalog/contacts.html')
