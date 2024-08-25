a7cd21e24810b755861d8b10fb8cb6ccb5a1586d
Aleksey Manzhikov <sanal-manzhikov@mail.ru>
2024-08-25 18:56:54 +0300

    init

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-
-from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy,reverse
 from blog.models import Blog
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from pytils.translit import slugify
@@ -20,7 +20,7 @@ class BlogCreateView(CreateView):
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'image')
-    success_url = reverse_lazy('blog:blog_list')
+
 
     def form_valid(self, form):
         if form.is_valid():
@@ -29,8 +29,12 @@ class BlogUpdateView(UpdateView):
             new_mat.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
+
 
 class BlogListView(ListView):
+    paginate_by: int = 6
     model = Blog
 
 class BlogDetailView(DetailView):
@@ -43,6 +47,7 @@ class BlogDetailView(DetailView):
         self.object.save()
         return self.object
 
+
 class BlogDeleteView(DeleteView):
     model = Blog
     fields = ('title', 'content', 'image')
@@ -50,3 +55,13 @@ class BlogDeleteView(DeleteView):
 
 
 
+def is_published(request, pk):
+    is_published_blog = get_object_or_404(Blog, pk=pk)
+    if is_published_blog:
+        is_published_blog = False
+    else:
+        is_published_blog = True
+
+    is_published_blog.save()
+
+    return redirect(reverse('blog:blog_list'))
